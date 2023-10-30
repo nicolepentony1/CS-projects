dacfb23999a0c597de4c85482a1189ebba345f85
nicolepentony1 <149390700+nicolepentony1@users.noreply.github.com>
2023-10-30 13:00:17 +0000

    Add files via upload

---
+++ assignment-01-21448212.js
@@ -0,0 +1,96 @@
+function incrementClick(top1)
+{
+    var element = document.getElementById(top1);
+    var value = parseInt(element.getAttribute("value"));
+    element.setAttribute("value",++value);
+    element.innerHTML = value;
+}
+function decrementClick(top1)
+{
+    var element = document.getElementById(top1);
+    var value = parseInt(element.getAttribute("value"));
+    if(value > 0){
+        element.setAttribute("value",--value);
+    element.innerHTML = value;
+    }
+}
+
+
+function incrementClick(block1)
+{
+    var element = document.getElementById(block1);
+    var value = parseInt(element.getAttribute("value"));
+    element.setAttribute("value",++value);
+    element.innerHTML = value;
+}
+function decrementClick(block1)
+{
+    var element = document.getElementById(block1);
+    var value = parseInt(element.getAttribute("value"));
+    element.setAttribute("value",--value);
+    element.innerHTML = value;
+}
+
+
+function incrementClick(block2)
+{
+    var element = document.getElementById(block2);
+    var value = parseInt(element.getAttribute("value"));
+    element.setAttribute("value",++value);
+    element.innerHTML = value;
+}
+function decrementClick(block2)
+{
+    var element = document.getElementById(block2);
+    var value = parseInt(element.getAttribute("value"));
+    element.setAttribute("value",--value);
+    element.innerHTML = value;
+}
+
+function incrementClick(block3)
+{
+    var element = document.getElementById(block3);
+    var value = parseInt(element.getAttribute("value"));
+    element.setAttribute("value",++value);
+    element.innerHTML = value;
+}
+function decrementClick(block3)
+{
+    var element = document.getElementById(block3);
+    var value = parseInt(element.getAttribute("value"));
+    element.setAttribute("value",--value);
+    element.innerHTML = value;
+}
+
+function incrementClick(block4)
+{
+    var element = document.getElementById(block4);
+    var value = parseInt(element.getAttribute("value"));
+    element.setAttribute("value",++value);
+    element.innerHTML = value;
+}
+function decrementClick(block4)
+{
+    var element = document.getElementById(block4);
+    var value = parseInt(element.getAttribute("value"));
+    element.setAttribute("value",--value);
+    element.innerHTML = value;
+}
+
+function incrementClick(block5)
+{
+    var element = document.getElementById(block5);
+    var value = parseInt(element.getAttribute("value"));
+    element.setAttribute("value",++value);
+    element.innerHTML = value;
+}
+function decrementClick(block5)
+{
+    var element = document.getElementById(block5);
+    var value = parseInt(element.getAttribute("value"));
+    element.setAttribute("value",--value);
+    element.innerHTML = value;
+}
+
+
+
